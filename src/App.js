72e7d6460d9e4a80ada6a05c0631a5fca151fa0c
touchngo <touchngo@TOUCHNGO>
2021-11-10 11:03:13 +0100

    Advice page created with styles, frontend of page completed with sample data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Advice from './pages/Advice';
 
 function App() {
   return (
     <div className="App">
-      
+    <div className="App-header">
+     <Advice/>
+    </div>
     </div>
   );
 }
